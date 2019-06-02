d1c2cf20432ce2866c7e548ef435c45f1ec86589
Marcus Sanatan <msanatan@gmail.com>
2019-06-02 17:13:35 -0400

    Add player-ball collision
    We needed to add some much needed X velocity for the game to be fun.
    
    Also added the missing collision for the other brick groups.

--- breakout.js
+++ breakout.js
@@ -68,8 +68,12 @@ function create() {
   player = this.physics.add.sprite(
     400, // x position
     600, // y position
-    'paddle' // key of image for the sprite
+    'paddle', // key of image for the sprite
   );
+
+  // Make the player immovable so the ball bounces off it
+  player.body.immovable = true;
+
   // Variable to track key presses
   cursors = this.input.keyboard.createCursorKeys();
   // Ensure that the player can't leave the screen
@@ -78,7 +82,7 @@ function create() {
   // Let's add the ball
   ball = this.physics.add.sprite(
     400, // x position
-    570, // y position
+    565, // y position
     'ball' // key of image for the sprite
   );
   ball.setBounce(1, 1);
@@ -125,7 +129,11 @@ function create() {
   this.physics.world.checkCollision.down = false;
 
   // Add collision for the bricks
-  this.physics.add.collider(ball, violetBricks, destroyBall, null, this);
+  this.physics.add.collider(ball, violetBricks, destroyBrick, null, this);
+  this.physics.add.collider(ball, yellowBricks, destroyBrick, null, this);
+  this.physics.add.collider(ball, redBricks, destroyBrick, null, this);
+  // Add collision for the player
+  this.physics.add.collider(ball, player, hitPaddle, null, this);
 }
 
 /**
@@ -139,9 +147,9 @@ function update() {
    * adjust velocity for movement as opposed to manipulating xy values directly
    */
   if (cursors.left.isDown) {
-    player.body.setVelocityX(-250);
+    player.body.setVelocityX(-350);
   } else if (cursors.right.isDown) {
-    player.body.setVelocityX(250);
+    player.body.setVelocityX(350);
   }
 
   // The game only begins when the user presses Spacebar to release the paddle
@@ -161,9 +169,39 @@ function update() {
  * In the create function, ball is a sprite and violetBricks, yellowBricks and
  * redBricks are sprite groups. Phaser is smart enough to handle the collisions
  * for each individual sprite.
- * @param ball - the ball sprite object
- * @param brick - the brick sprite object
+ * @param ball - the ball sprite
+ * @param brick - the brick sprite
  */
-function destroyBall(ball, brick) {
+function destroyBrick(ball, brick) {
   brick.destroy();
+
+  if (ball.body.velocity.x == 0) {
+    randNum = Math.random();
+    if (randNum >= 0.5) {
+      ball.body.setVelocityX(150);
+    } else {
+      ball.body.setVelocityX(-150);
+    }
+  }
+}
+
+/**
+ * The function handles the collision between the ball and the player. We want
+ * to ensure that the ball's direction after bouncing off the player is based
+ * on which side of the player was hit. Also, to make things more difficult, we
+ * want to increase the ball's velocity when it's hit.
+ * @param ball - the ball sprite
+ * @param player - the player/paddle sprite
+ */
+function hitPaddle(ball, player) {
+  // Increase the velocity of the ball after it bounces
+  ball.body.setVelocityY(ball.body.velocity.y - 15);
+
+  let newXVelocity = Math.abs(ball.body.velocity.x) + 15;
+  // If the ball is to the left of the player, ensure the x velocity is negative
+  if (ball.body.x < player.body.x) {
+    ball.body.setVelocityX(-newXVelocity);
+  } else {
+    ball.body.setVelocityX(newXVelocity);
+  }
 }
